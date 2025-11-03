5ac5a19ee5c0d61add4f3f5b375ae75bb6ab0d4d
Agustina Insfran <agusinsfran08@gmail.com>
2025-11-03 00:30:42 -0300

    fix: Corregir archivo vite

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,25 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-import { resolve } from 'path'
-import { fileURLToPath, URL } from 'url' 
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-
-const __dirname = fileURLToPath(new URL('.', import.meta.url))
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  root: '.', 
   build: {
+    outDir: 'dist',
     rollupOptions: {
       input: {
-
         main: resolve(__dirname, 'index.html'),
         palas: resolve(__dirname, 'palas.html'),
         ropa: resolve(__dirname, 'ropa.html'),
         accesorios: resolve(__dirname, 'accesorios.html'),
-        zapatillas: resolve(__dirname, 'zapatillas.html')
-
-      }
-    }
-  }
+        zapatillas: resolve(__dirname, 'zapatillas.html'),
+      },
+    },
+  },
 })
