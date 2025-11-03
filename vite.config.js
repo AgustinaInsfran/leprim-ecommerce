56da72265f1f51b74bdb1ae31b778091b4543dcc
Agustina Insfran <agusinsfran08@gmail.com>
2025-11-03 00:16:32 -0300

    Actualización vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,24 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path' 
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+
+  build: {
+    rollupOptions: {
+      input: {
+
+        main: resolve(__dirname, 'index.html'),
+        palas: resolve(__dirname, 'palas.html'),
+        ropa: resolve(__dirname, 'ropa.html'),
+        accesorios: resolve(__dirname, 'accesorios.html'),
+        zapatillas: resolve(__dirname, 'zapatillas.html')
+
+      }
+    }
+  }
+
 })
